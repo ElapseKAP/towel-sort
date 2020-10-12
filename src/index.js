9d11f98f2eacaba9c9d2552500b3f090cc8987e4
Alexander K <elapse790527@yahoo.co.uk>
2020-10-12 17:17:35 +0300

    feat: added sort implementation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!Array.isArray(matrix) || matrix.length == 0) {
+    return [];
+  }
+
+  let newArr = [],
+    sortArr;
+
+  for (let i = 0; i < matrix.length; i++) {
+
+    if (i%2 !== 0) {
+      sortArr = matrix[i].sort(function(a, b) {
+        return b - a;
+      });
+      newArr = newArr.concat(sortArr);
+      continue;
+    }
+
+    newArr = newArr.concat(matrix[i]);
+  }
+
+  return newArr;
 }
